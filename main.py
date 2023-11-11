7fffec5d8dd5e42c0fea40a05c1a5f48fbeb8e69
JKim171 <110058814+JKim171@users.noreply.github.com>
2023-11-11 12:45:57 -0600

    Adding Titles and Button

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import tkinter as tk
+from tkinter import font
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
 if __name__ == '__main__':
     root = ttk.Window(title="Dining Hall Calculator", themename="solar", size=(1920,1080))
 
-    b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
-    b1.pack(side=LEFT, padx=5, pady=10)
+    diningHallLabel = ttk.Label(root, text="Dining Hall:", bootstyle="solar", font=font.Font(family="Arial", size=34))
+    diningHallLabel.pack(side="top", anchor="nw", margin)
 
-    b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
-    b2.pack(side=LEFT, padx=5, pady=10)
+    rhetasButton = ttk.Button(root, text="Rheta's Market", style="info")
+    rhetasButton.pack(side="top", anchor="nw")
 
     root.mainloop()
-
 
