c7e6e1fc63ffb6c432ebabb70e57756be294795a
JKim171 <110058814+JKim171@users.noreply.github.com>
2023-11-12 11:50:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,5 @@ from gui import GUI
 from scrape import Scrape
 
 if __name__ == '__main__':
-    #GUI.runGUI()
-    Scrape.runScrape('https://wisc-housingdining.nutrislice.com/menu/gordon-avenue-market/breakfast/2023-11-11')
-    Scrape.runScrape('https://wisc-housingdining.nutrislice.com/menu/four-lakes-market/breakfast/2023-11-11')
-    Scrape.runScrape('https://wisc-housingdining.nutrislice.com/menu/carsons-market/lunch/2023-11-11')
-    Scrape.runScrape('https://wisc-housingdining.nutrislice.com/menu/lizs-market/breakfast/2023-11-11')
-    Scrape.runScrape('https://wisc-housingdining.nutrislice.com/menu/rhetas-market/breakfast/2023-11-11')
+    GUI.runGUI()
+
